b78170d4df7ad23283b66f68476716890dbb3d27
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-14 12:26:22 +0100

    libft - improve ft_isalpha.c

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,18 +6,21 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:21:37 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/13 13:44:47 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/14 12:25:03 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
-ft_isalpha()
-
+ft_isalpha() :
 checks  for an alphabetic character;
 in the standard "C" locale, it is equivalent to (isupper(c) || islower(c)).
 In some locales, there may be  additional characters for which isalpha()
 is true—letters which are neither uppercase nor lowercase.
 */
+
+// Deliberatly choose to not call ft_islower and ft_isupper and duplicate code
+// Because I want my libft efficient
+// and avoid the overhead cost of calling two external fonction.
 
 int	ft_isalpha(int c)
 {
@@ -26,16 +29,3 @@ int	ft_isalpha(int c)
 	else
 		return (0);
 }
-
-/* 
-#include <stdio.h>
-int main(void)
-{
-	char *str = "ajQT09,.1-]\\~";
-	while (*str)
-	{
-		printf("%c : %d\n",*str,ft_isalpha(*str));
-		str++;
-	}
-}
- */
