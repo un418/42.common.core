757b327f94431ea3fc555646be0434e0c04fc548
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-14 13:29:52 +0100

    libft - add ft_isprint.c

--- libft/test.c
+++ libft/test.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:53:21 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/14 13:18:33 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/14 13:27:57 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ int main(void)
 	}
 
 	// ft_isascii()
-	if (1 || test_all)
+	if (0 || test_all)
 	{
 		printf("--- TEST for ft_isascii() ---\n");
 		char *str = "ajQT09 #/=@-\\~";
@@ -67,6 +67,22 @@ int main(void)
 			str++;
 		}
 		printf("%d : %d\n",-8,ft_isascii(-8));
+		printf("---------\n");
+	}
+
+	// ft_isprint()
+	if (1 || test_all)
+	{
+		printf("--- TEST for ft_isprint() ---\n");
+		char *str = "ajQT09 #/=@-\\~";
+		while (*str)
+		{
+			printf("%c : %d\n",*str,ft_isprint(*str));
+			str++;
+		}
+		printf("%d : %d\n",-8,ft_isprint(-8));
+		printf("TAB : %d\n",ft_isprint('	'));
+		printf("NULL : %d\n",ft_isprint(0));
 		printf("---------\n");
 	}
 }
