04748caf823e48410aeb4e4d18bf4d2464cefa8b
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-14 12:56:06 +0100

    libft - add ft_isdigit.c

--- libft/test.c
+++ libft/test.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:53:21 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/13 14:25:45 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/14 12:53:17 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,32 @@
 #include <stdio.h>
 int main(void)
 {
+	int test_all = 0;
 
-	// ft_isalpha()
-	printf("--- TEST for ft_isalpha() ---\n");
-	char *str = "ajQT09,.1-]\\~";
-	while (*str)
+	if (0 || test_all)
 	{
-		printf("%c : %d\n",*str,ft_isalpha(*str));
-		str++;
+		// ft_isalpha()
+		printf("--- TEST for ft_isalpha() ---\n");
+		char *str = "ajQT09,.1-]\\~";
+		while (*str)
+		{
+			printf("%c : %d\n",*str,ft_isalpha(*str));
+			str++;
+		}
+		printf("---------\n");
 	}
-	printf("---------\n");
+
+	if (1 || test_all)
+	{
+		// ft_isdigit()
+		printf("--- TEST for ft_isdigit() ---\n");
+		char *str = "ajQT09 #/=@-\\~";
+		while (*str)
+		{
+			printf("%c : %d\n",*str,ft_isdigit(*str));
+			str++;
+		}
+		printf("---------\n");
+	}
 }
 
