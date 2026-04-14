49319958a5124452349b352e70778204c6160079
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-14 13:29:52 +0100

    add libft/ft_isprint.c

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/14 12:22:22 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/14 13:24:06 by adaferna         ###   ########.fr       */
+/*   Created: 2026/04/14 13:20:54 by adaferna          #+#    #+#             */
+/*   Updated: 2026/04/14 13:28:51 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+/* 
+isprint()
+checks for any printable character including space.
+*/
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	if (32 <= c && c <= 126)
+		return (1);
+	else
+		return (0);
+}
