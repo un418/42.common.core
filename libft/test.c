4cbf6a58df231116095d00e21e95b6ea6349ff22
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-16 19:32:28 +0100

    libft - add ft_tolower.c

--- libft/test.c
+++ libft/test.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:53:21 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/16 19:18:11 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/16 19:29:43 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -277,8 +277,30 @@ int main(void)
 			printf("after:%d \n", c);
 		}
 		printf("---------\n");
-
-
-
 	}
+
+	// ft_tolower()
+	if (1 || test_all)
+	{
+		printf("--- TEST for ft_tolower() ---\n");
+		printf("Test1 ---\n");
+		char c = 'a';
+		printf("before:%c \n", c);
+		printf("before:%d \n", c);
+		c = ft_tolower(c);
+		printf("after:%c \n",c);
+		printf("after:%d \n", c);
+
+		{
+			printf("Test2 ---\n");
+			char c = 'E';
+			printf("before:%c \n", c);
+			printf("before:%d \n", c);
+			c = ft_tolower((int)c);
+			printf("after:%c \n",c);
+			printf("after:%d \n", c);
+		}
+		printf("---------\n");
+	}
+
 }
