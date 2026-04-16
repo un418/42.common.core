569c90fe82cdcb078d59711bd7be42ca412e8467
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-16 18:43:10 +0100

    add libft/ft_strlcpy.c

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 16:35:07 by adaferna          #+#    #+#             */
+/*   Updated: 2026/04/16 18:37:21 by adaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+NAME
+strlcpy — size-bounded string copying and concatenation
+
+LIBRARY
+Utility functions from BSD systems (libbsd, -lbsd)
+
+SYNOPSIS
+#include <string.h>
+(See libbsd(7) for include usage.)
+
+size_t strlcpy(char *dst, const char *src, size_t size);
+DESCRIPTION
+The strlcpy()functions copy strings.
+Designed to be safer, more consistent,
+and less error prone replacements for strncpy(3).
+Unlike those functions, strlcpy() take the full size of the buffer
+(not just the length) and guarantee to
+NUL-terminate the result (as long as size is larger than 0.
+Note that a byte for the NUL should be included in size.
+Also note that strlcpy() only operate on true “C” strings.
+This means that for strlcpy() src must be NUL-terminated.
+
+The strlcpy() function copies up to size - 1 characters
+from the NUL-terminated string src to dst, NUL-terminating the result.
+
+
+RETURN VALUES
+The strlcpy()functions return the total length
+of the string they tried to create.
+For strlcpy() that means the length of src.
+
+While this may seem somewhat confusing,
+it was done to make truncation detection simple.
+*/
+
+// 
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	const char *p_src;
+
+	p_src = src;
+	if ( size != 0)
+	{
+		while (--size && *src)
+			*dst++ = *src++;
+		*dst = '\0';
+	}
+	return(ft_strlen(p_src));
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 12:22:22 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/15 14:01:49 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/16 17:11:27 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
