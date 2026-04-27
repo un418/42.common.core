0f164e7072770d7276ea682f04f7536806c878bf
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 00:54:42 +0100

    libft - improve comments

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,25 +6,21 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 11:16:22 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/15 12:55:11 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 00:55:08 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
 NAME
-memcpy - copy memory area
-
-SYNOPSIS
-include <string.h>
-void *memcpy(void *dest, const void *src, size_t n);
+ memcpy - copy memory area
 
 DESCRIPTION
-The memcpy() function copies n bytes from memory area src to memory area dest.
-The memory areas must not overlap.
-Use memmove(3) if the memory areas do overlap.
+ The memcpy() function copies n bytes from memory area src to memory area dest.
+ If dst and src overlap, behavior is undefined.
+ Use memmove(3) if the memory areas do overlap.
 
 RETURN VALUE
-The memcpy() function returns a pointer to dest.
+ The memcpy() function returns the original value of dst.
 */
 
 #include "libft.h"
