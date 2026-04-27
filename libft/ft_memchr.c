e49309da57094d88f8f22cb1d4dd8d882c18ad5e
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 00:43:29 +0100

    libft - improve comments

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,26 +6,21 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 17:20:27 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/17 18:13:58 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 00:38:59 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
 NAME
-memchr - scan memory for a character
-
-SYNOPSIS
-void *memchr(const void *s, int c, size_t n);
+ memchr - scan memory for a character
 
 DESCRIPTION
-The  memchr()  function scans the initial n bytes of the memory area
-pointed to by s for the first instance of c.
-Both c and the bytes of the memory area pointed to by s
-are interpreted as unsigned char.
+ The memchr() function locates the first occurrence 
+  of c (converted to an unsigned char) in string s.
 
 RETURN VALUE
-The memchr() functions return a pointer to the matching byte
-or NULL if the character does not occur in the given memory area.
+ The memchr() function returns a pointer to the byte located,
+  or NULL if no such byte exists within n bytes.
 */
 
 #include "libft.h"
