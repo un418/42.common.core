e498cd58f0d3aff3be07e78bfa456139706338de
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 22:44:14 +0100

    libft - fix norm & comment

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 19:44:16 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/28 20:17:33 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 22:43:26 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@ NAME
  ft_strchr - locate first occurrence of the character in string
 
 DESCRIPTION
- The strchr() function locates the first occurrence of c
+ The ft_strchr() function locates the first occurrence of c
   (converted to a char) in the string pointed to by s.
  The terminating null character is considered part of the string,
   therefore if c is ‘\0’, the functions locate the terminating ‘\0’.
 
 RETURN VALUE
- - Return a pointer to the first match of the character
+ - Return a pointer to the first occurrence of the character
  - Return NULL if the character is not found
 */
 
@@ -33,7 +33,7 @@ char	*ft_strchr(const char *s, int c)
 	{
 		if (*s == (char)c)
 			return ((char *)s);
-		if (*s =='\0')
+		if (*s == '\0')
 			return (NULL);
 		s++;
 	}
