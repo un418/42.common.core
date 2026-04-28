d41fc167e36fe2f3b0909808937fd80988ff68f0
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 18:28:22 +0100

    libft - improve comments

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,13 +6,18 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 13:20:54 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/14 14:52:27 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 18:28:02 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
-isprint()
-checks for any printable character including space.
+NAME
+ ft_isprint() - checks for printable character
+DESCRIPTION
+ Checks for any printable character including space.
+RETURN VALUE
+ - 0 if test fail
+ - non-zero if test pass
 */
 
 #include "libft.h"
