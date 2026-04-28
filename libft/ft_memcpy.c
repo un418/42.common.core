9ffe7e94158e20749d1d98c613a0fb1d5d57d8b8
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 22:55:22 +0100

    libft - improve casting & comment

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,31 +6,34 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 11:16:22 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/28 01:11:31 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 23:03:28 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
 NAME
- memcpy - copy memory area
+ ft_memcpy - copy memory area
 
 DESCRIPTION
- The memcpy() function copies n bytes from memory area src to memory area dest.
+ The ft_memcpy() function copies n bytes from memory area src
+  to memory area dest.
  If dst and src overlap, behavior is undefined.
  Use memmove(3) if the memory areas do overlap.
 
 RETURN VALUE
- The memcpy() function returns the original value of dst.
+ - The original value of dest.
 */
 
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*ptr;
+	unsigned char		*p_dest;
+	const unsigned char	*p_src;
 
-	ptr = dest;
+	p_dest = (unsigned char *)dest;
+	p_src = (const unsigned char *)src;
 	while (n--)
-		*ptr++ = *(char *)src++;
+		*p_dest++ = *p_src++;
 	return (dest);
 }
