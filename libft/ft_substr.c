6c34c2de3fd85088700b114d4b3d7cc4e7e07c1d
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 19:17:52 +0100

    libft - improve comments and use malloc instead of calloc to improve perf

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,24 +6,27 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 19:55:16 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/22 21:04:46 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 19:16:52 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
-ft_substr() - Create a substring of `len` size.
-
-PARAMETERS
-- s: The original string from which to create the substring.
-- start: The starting index of the substring within ’s’.
-- len: The maximum length of the substring.
-RETURN VALUE
-The substring.
-NULL if the allocation fails.
+NAME
+ ft_substr() - Create a substring of `len` size
 
 DESCRIPTION
-Allocates memory (using malloc(3)) and returns a substring from the string ’s’.
-The substring starts at index ’start’ and has a maximum length of ’len’.
+ Allocates memory (using malloc()) and returns a substring from the string ’s’.
+ The substring starts at index ’start’ and has a maximum length of ’len’.
+
+PARAMETERS
+ - s: The original string from which to create the substring.
+ - start: The starting index of the substring within ’s’.
+ - len: The maximum length of the substring.
+
+RETURN VALUE
+ - Pointer to the beginning of the substring
+ - NULL if the allocation fails.
+ - In case of error return a pointer to a empty string
 */
 
 #include "libft.h"
@@ -38,9 +41,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (len > len_s - start)
 		len = len_s - start;
-	p_sub = ft_calloc(len + 1, sizeof(char));
+	p_sub = malloc((len + 1) * sizeof(char));
 	if (!p_sub)
 		return (NULL);
-	ft_strlcpy(p_sub, (s + start), len + 1);
+	ft_strlcpy(p_sub, (s + start), (len + 1));
 	return (p_sub);
 }
