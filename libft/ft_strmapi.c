8512d48ce4c3a6ad3422a495d0881dba5333d07e
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 21:20:58 +0100

    libft - improve comments & use malloc instead of calloc for perf

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,12 +6,14 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 13:20:02 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/25 15:41:31 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 21:20:12 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-ft_strmapi() - Create string by apllying function to each character of a string
+NAME
+ ft_strmapi() - Create a copy of a string by applying function f
+  to each character of a string
 
 DESCRIPTION
  Applies the function f to each character of the string s,
@@ -20,9 +22,12 @@ DESCRIPTION
   from the successive applications of f.
  Also note that ft_strmapi() only operate on true “C” strings.
  This means that for ft_strmapi() `*s` must be NUL-terminated.
+
 PARAMETERS
  - s: The string to iterate over.
  - f: The function to apply to each character.
+
+
 RETURN VALUE
  - The string created from the successive applications of ’f’.
  - Returns NULL if the allocation fails.
@@ -38,7 +43,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	if (!s)
 		return (NULL);
 	i = 0;
-	str = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	str = malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	while (s[i])
@@ -46,5 +51,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		str[i] = f(i, s[i]);
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
