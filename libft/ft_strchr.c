ae74e4686f17d84c956404a96c6cf1a534c5dcc2
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 20:19:09 +0100

    libft - improve code logic & comments

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,49 +6,35 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 19:44:16 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/22 17:41:56 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 20:17:33 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
 NAME
-strchr - locate first occurrence of the character in string
-
-SYNOPSIS
-
-char *strchr(const char *s, int c);
-
+ ft_strchr - locate first occurrence of the character in string
 
 DESCRIPTION
-The strchr() function returns a pointer 
-to the first occurrence of the character c in the string s.
-
-Here "character" means "byte";
-these functions do not work with wide or multibyte characters.
+ The strchr() function locates the first occurrence of c
+  (converted to a char) in the string pointed to by s.
+ The terminating null character is considered part of the string,
+  therefore if c is ‘\0’, the functions locate the terminating ‘\0’.
 
 RETURN VALUE
-The  strchr()functions return a pointer to the matched character
-or NULL if the character is not found.
-The terminating null byte is considered part of the string,
-so that if c is specified as '\0',
-these functions re‐turn a pointer to the terminator.
+ - Return a pointer to the first match of the character
+ - Return NULL if the character is not found
 */
 
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	char	cc;
-
-	cc = (char)c;
-	while (*s)
+	while (1)
 	{
-		if (*s == cc)
+		if (*s == (char)c)
 			return ((char *)s);
+		if (*s =='\0')
+			return (NULL);
 		s++;
 	}
-	if (cc == 0)
-		return ((char *)s);
-	else
-		return (NULL);
 }
