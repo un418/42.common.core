fbd79b07fb49b53c388631b9feb490bf3d6df46d
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 21:12:11 +0100

    libft - improve comments

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,16 +6,20 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 13:30:40 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/14 15:37:29 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 21:11:52 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
+NAME
+ ft_strlen() - calculate the length of a string
+
 DESCRIPTION
-The strlen() function calculates the length of the string pointed to by s,
-excluding the terminating null byte ('\0').
+ The ft_strlen() function calculates the length of the string pointed to by s,
+  excluding the terminating null byte ('\0').
+
 RETURN VALUE
-The strlen() function returns the number of bytes in the string pointed to by s.
+ - The number of characters that precede the terminating NUL character
 */
 
 #include "libft.h"
