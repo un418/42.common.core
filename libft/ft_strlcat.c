cab8e83049faca99cf7b39946e0c57e3c78e1ddc
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-28 20:47:17 +0100

    libft - improve comments

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,30 +6,26 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 19:50:50 by adaferna          #+#    #+#             */
-/*   Updated: 2026/04/22 16:19:47 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/04/28 20:46:42 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
 NAME
-strlcat – size-bounded string concatenation
-
-
-SYNOPSIS
-size_t  strlcat(char *dst, const char *src, size_t size);
+ ft_strlcat – size-bounded string concatenation
 
 DESCRIPTION
-The strlcat() functions concatenate strings.
+The ft_strlcat() functions concatenate strings.
 Designed to be safer, more consistent, and less error prone replacements
-for the easily misused functions strncpy(3) and strncat(3).
+ for the easily misused functions strncpy(3) and strncat(3).
 
-strlcat() take the full size of the destination buffer
+ft_strlcat() take the full size of the destination buffer
 and guarantee NUL-termination if there is room.
 Note that room for the NUL should be included in dstsize.
-Also note that strlcat() only operate on true ''C'' strings.
-This means that for strlcat() both src and dst() must be NUL-terminated.
+Also note that ft_strlcat() only operate on true ''C'' strings.
+This means that both src and dst() must be NUL-terminated.
 
-strlcat() appends string src to the end of dst.
+ft_strlcat() appends string src to the end of dst.
 It will append at most size - strlen(dst) - 1 bytes, NUL-terminating the result.
 It will then NUL-terminate, unless dstsize is 0 or the original dst string was
 longer than dstsize (in practice this should not happen as it means
@@ -38,11 +34,10 @@ that either dstsize is incorrect or that dst is not a proper string).
 If the src and dst strings overlap, the behavior is undefined.
 
 RETURN VALUES
-strlcat() functions return the total length of the string it tried to create,
-means the initial length of dst plus the length of src.
-
-If the return value is >= dstsize, the output string has been truncated.
-It is the caller's responsibility to handle this.
+ - The total length of the string it tried to create,
+   means the initial length of dst plus the length of src.
+ - If the return value is >= dstsize, the output string has been truncated.
+   It is the caller's responsibility to handle this.
 */
 
 #include "libft.h"
