2e89330976b0b84b6a3122d35148cbe5f322e148
Adam Fernandes <adaferna@student.42lisboa.com>
2026-05-11 12:01:49 +0100

    get_next_line - add possibility to set macro from compiler option

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,15 +6,20 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 16:05:58 by adaferna          #+#    #+#             */
-/*   Updated: 2026/05/09 01:30:11 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/05/09 23:34:13 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GNL_H
 # define GNL_H
 
-# define BUFFER_SIZE 2
-# define DELIMITER '\n'
+# ifndef BUFFER_SIZE
+ # define BUFFER_SIZE 100
+# endif
+
+# ifndef DELIMITER
+ # define DELIMITER '\n'
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -22,7 +27,7 @@
 char	*get_next_line(int fd);
 
 // utils
-void	*ft_strnchr(const void *s, int c, size_t n);
+void	*ft_strnchr(const void *s, int c, size_t n, int file_end);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
