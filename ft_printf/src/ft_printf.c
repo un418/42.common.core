73a6022d91626324cc17fe04fb207c31336a0f39
Adam Fernandes <adaferna@student.42lisboa.com>
2026-05-14 17:21:33 +0100

    ft_printf - improve usage of fd

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,35 +6,55 @@
 /*   By: adaferna <adaferna@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 19:56:29 by adaferna          #+#    #+#             */
-/*   Updated: 2026/05/06 16:43:57 by adaferna         ###   ########.fr       */
+/*   Updated: 2026/05/14 17:20:09 by adaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	ft_printf_switch(va_list args, const char *str)
+static size_t	ft_dprintf_switch(int fd, va_list args, const char *str)
 {
 	size_t	counter;
 
 	counter = 0;
 	if (*str == 'c')
-		return (ft_write_fd_char(va_arg(args, int), 1));
+		return (ft_write_fd_char(va_arg(args, int), fd));
 	else if (*str == '%')
-		return (ft_write_fd_char('%', 1));
+		return (ft_write_fd_char('%', fd));
 	else if (*str == 's')
-		return (ft_write_fd_str(va_arg(args, char *), 1));
+		return (ft_write_fd_str(va_arg(args, char *), fd));
 	else if (*str == 'd' || *str == 'i')
-		return (ft_writesnbr_fd(va_arg(args, int), 1));
+		return (ft_writesnbr_fd(va_arg(args, int), fd));
 	else if (*str == 'u')
-		return (ft_writeunbr_fd_recurse(va_arg(args, unsigned int), 1));
+		return (ft_writeunbr_fd_recurse(va_arg(args, unsigned int), fd));
 	else if (*str == 'x')
-		return (ft_writehex_fd_recurse(va_arg(args, unsigned int), 1, 0));
+		return (ft_writehex_fd_recurse(va_arg(args, unsigned int), fd, 0));
 	else if (*str == 'X')
-		return (ft_writehex_fd_recurse(va_arg(args, unsigned int), 1, 1));
+		return (ft_writehex_fd_recurse(va_arg(args, unsigned int), fd, 1));
 	else if (*str == 'p')
-		return (ft_write_fd_pointer(va_arg(args, void *), 1));
+		return (ft_write_fd_pointer(va_arg(args, void *), fd));
 	else
 		return (counter);
+}
+
+static int	ft_dprintf(int fd, const char *str, va_list args)
+{
+	size_t	count;
+
+	count = 0;
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			count += ft_dprintf_switch(fd, args, str);
+			str++;
+			continue ;
+		}
+		count += ft_write_fd_char(*str, fd);
+		str++;
+	}
+	return (count);
 }
 
 int	ft_printf(const char *str, ...)
@@ -44,18 +64,7 @@ int	ft_printf(const char *str, ...)
 
 	count = 0;
 	va_start(args, str);
-	while (*str)
-	{
-		if (*str == '%')
-		{
-			str++;
-			count += ft_printf_switch(args, str);
-			str++;
-			continue ;
-		}
-		count += ft_write_fd_char(*str, 1);
-		str++;
-	}
+	count = ft_dprintf(1, str, args);
 	va_end(args);
 	return (count);
 }
