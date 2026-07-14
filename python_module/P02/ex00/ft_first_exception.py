c53af973bf7d66a32b7a2dfb2eb1a4ce33a545e9
Adam Fernandes <adaferna@student.42lisboa.com>
2026-07-14 02:12:38 +0100

    python_module 02 - feat: add ex0

---
+++ python_module/P02/ex00/ft_first_exception.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
+
+
+def test_temperature() -> None:
+    data = ['25', 'abc']
+
+    for input in data:
+        print(f"Input data is '{input}'")
+        try:
+            output = input_temperature(input)
+            print(f"Temperature is now {output}°C")
+        except Exception as e:
+            print(f"Caught input_temperature error: {e}")
+        finally:
+            print()
+
+
+def main() -> None:
+    print("=== Garden Temperature ===\n")
+    test_temperature()
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    main()
