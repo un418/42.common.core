4b55797bc579fba578457b050371f8320290d3a1
Adam Fernandes <adaferna@student.42lisboa.com>
2026-07-15 19:35:26 +0100

    python_module 02 - fix: ex1 use clause guard pattern for more idiomatic code

--- python_module/P02/ex1/ft_raise_exception.py
+++ python_module/P02/ex1/ft_raise_exception.py
@@ -4,10 +4,9 @@ def input_temperature(temp_str: str) -> int:
     temp = int(temp_str)
     if temp < 0:
         raise ValueError(f"{temp}°C is too cold for plants (min 0°C)")
-    elif temp > 40:
+    if temp > 40:
         raise ValueError(f"{temp}°C is too hot for plants (max 40°C)")
-    else:
-        return temp
+    return temp
 
 
 def test_temperature() -> None:
