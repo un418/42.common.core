55263c7be3d86047852202ae0dcf8f83864c7370
Adam Fernandes <adaferna@student.42lisboa.com>
2026-07-30 15:01:52 +0100

    python_module 05 - feat: ex0: add more test in main()

--- python_module/P05/ex0/data_processor.py
+++ python_module/P05/ex0/data_processor.py
@@ -35,7 +35,6 @@ class NumericProcessor(DataProcessor):
 
     def ingest(self, data: int | float | list[int] | list[int | float]
                ) -> None:
-        # list[int | float] = [1.0, 2, 4.7] = list[float]
         if not self.validate(data):
             raise TypeError(self.ERR_MSG)
         items = data if isinstance(data, list) else [data]
@@ -106,7 +105,7 @@ def main() -> None:
     print("Testing Numeric Processor...")
     np0 = NumericProcessor()
     # Validation
-    for val_np in (42, "Hello", True):
+    for val_np in (42, 4.2, "Hello", True, [], [24, 2.4, "error"], [4, 2]):
         print(f" Trying to validate input '{val_np}': {np0.validate(val_np)}")
     # Ingestion error
     try:
@@ -115,12 +114,13 @@ def main() -> None:
     except Exception as e:
         print(f" Got exception: {e}")
     # Ingestion OK
-    try:
-        np0_data = [1, 2, 3, 4, 5]
-        print(f" Processing data: {np0_data}")
-        np0.ingest(np0_data)
-    except Exception as e:
-        print(f" Got exception: {e}")
+    np0_data = ([1, 2, 3, 4, 5], 42, 4.2, [24, 2.4])
+    for np0_d in np0_data:
+        try:
+            print(f" Processing data: {np0_d}")
+            np0.ingest(np0_d)
+        except Exception as e:
+            print(f" Got exception: {e}")
     # Extraction
     s = "" if len(np0) == 1 else "s"
     print(f" Extracting {len(np0)} value{s}...")
@@ -132,7 +132,7 @@ def main() -> None:
     print("Testing Text Processor...")
     tp0 = TextProcessor()
     # Validation
-    for val_tp in (42, "Hello"):
+    for val_tp in (42, "Hello", [], ["ko", []]):
         print(f" Trying to validate input '{val_tp}': {tp0.validate(val_tp)}")
     # Ingestion error
     try:
@@ -142,12 +142,13 @@ def main() -> None:
     except Exception as e:
         print(f" Got exception: {e}")
     # Ingestion OK
-    try:
-        tp0_data = ["Hello", "Nexus", "World"]
-        print(f" Processing data: {tp0_data}")
-        tp0.ingest(tp0_data)
-    except Exception as e:
-        print(f" Got exception: {e}")
+    tp0_data = (["Hello", "Nexus", "World"], "scalar")
+    for tp0_d in tp0_data:
+        try:
+            print(f" Processing data: {tp0_d}")
+            tp0.ingest(tp0_d)
+        except Exception as e:
+            print(f" Got exception: {e}")
     # Extraction
     s = "" if len(tp0) == 1 else "s"
     print(f" Extracting {len(tp0)} value{s}...")
@@ -164,7 +165,9 @@ def main() -> None:
         {"log_level": "DEBUG", "log_message": "i'm a valid test"},
         {404: "ERROR", "log_message": "invalid test"},
         {},
-        {"foo": "bar"}
+        {"foo": "bar"},
+        [{"42": 24.42}],
+        ()
     ):
         print(f" Trying to validate input '{val_lp}': {lp0.validate(val_lp)}")
     # Ingestion error
@@ -177,15 +180,17 @@ def main() -> None:
     except Exception as e:
         print(f" Got exception: {e}")
     # Ingestion OK
-    try:
-        lp0_data = [
-            {"log_level": "NOTICE", "log_message": "Connection to server"},
-            {"log_level": "ERROR", "log_message": "Unauthorized access!!"}
-        ]
-        print(f" Processing data: {lp0_data}")
-        lp0.ingest(lp0_data)
-    except Exception as e:
-        print(f" Got exception: {e}")
+    lp0_data = (
+        [{"log_level": "NOTICE", "log_message": "Connection to server"},
+         {"log_level": "ERROR", "log_message": "Unauthorized access!!"}],
+        {"log_level": "INFO", "log_message": "Scalar input"}
+    )
+    for lp0_d in lp0_data:
+        try:
+            print(f" Processing data: {lp0_d}")
+            lp0.ingest(lp0_d)
+        except Exception as e:
+            print(f" Got exception: {e}")
     # Extraction
     s = "" if len(lp0) == 1 else "s"
     print(f" Extracting {len(lp0)} value{s}...")
