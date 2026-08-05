59c4b1b9b0489177ca3e4e11091e0205318a7c6c
Adam Fernandes <adaferna@student.42lisboa.com>
2026-08-05 17:44:04 +0100

    python_module 04 - feat: ex4: improve code logic by using match/case pattern + improve test for linux env

--- python_module/P04/ex3/ft_vault_security.py
+++ python_module/P04/ex3/ft_vault_security.py
@@ -2,22 +2,27 @@
 
 def secure_archive(filename: str, mode: str = "r",
                    w_content: str = "") -> tuple[bool, str]:
-    if mode not in {"r", "w"}:
-        return (False, f"Error: mode {mode} not recognized - 'r' or 'w' only")
-    if mode == "w" and not w_content:
-        return (False, "Error: 'write' mode selected but content is empty")
     try:
         with open(filename, mode) as f:
-            if mode == "r":
-                r_content = f.read()
-                ret = r_content
-            if mode == "w":
-                f.write(w_content)
-                ret = "Content successfully written to file"
+            match mode:
+                case "r":
+                    r_content = f.read()
+                    return (True, r_content)
+                case "w":
+                    if not w_content:
+                        return (
+                            False,
+                            "Error: 'write' mode selected but content is empty"
+                            )
+                    f.write(w_content)
+                    return (True, 'Content successfully written to file')
+                case _:
+                    return (
+                        False,
+                        f"Error: mode {mode} not recognized - 'r' or 'w' only"
+                        )
     except OSError as e:
         return (False, f"{e}")
-    else:
-        return (True, ret)
 
 
 def main() -> None:
@@ -29,11 +34,11 @@ def main() -> None:
     print()
 
     print("Using 'secure_archive' to read from an inaccessible file:")
-    print(secure_archive("/etc/master.passwd", "r"))
+    print(secure_archive("/etc/passwd", "r"))
     print()
 
     print("Using 'secure_archive' to read from a regular file:")
-    memory = secure_archive("../test_subject.txt", "r")
+    memory = secure_archive("../test.txt", "r")
     print(memory)
     print()
 
