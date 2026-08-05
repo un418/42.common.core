6bc7b8661c76a1d2429341090ef276e3397e11ec
Adam Fernandes <adaferna@student.42lisboa.com>
2026-08-05 20:10:18 +0100

    python_module 04 - feat: improve logic and test on ex4

--- python_module/P04/ex3/ft_vault_security.py
+++ python_module/P04/ex3/ft_vault_security.py
@@ -3,24 +3,22 @@
 def secure_archive(filename: str, mode: str = "r",
                    w_content: str = "") -> tuple[bool, str]:
     try:
-        with open(filename, mode) as f:
-            match mode:
-                case "r":
+        match mode:
+            case "r":
+                with open(filename, mode) as f:
                     r_content = f.read()
-                    return (True, r_content)
-                case "w":
-                    if not w_content:
-                        return (
-                            False,
-                            "Error: 'write' mode selected but content is empty"
-                            )
-                    f.write(w_content)
-                    return (True, 'Content successfully written to file')
-                case _:
+                return (True, r_content)
+            case "w":
+                if not w_content:
                     return (
                         False,
-                        f"Error: mode {mode} not recognized - 'r' or 'w' only"
-                        )
+                        "Error: 'write' mode selected but content is empty")
+                with open(filename, mode) as f:
+                    f.write(w_content)
+                return (True, 'Content successfully written to file')
+            case _:
+                return (False,
+                        f"Error: mode {mode} not recognized - 'r' or 'w' only")
     except OSError as e:
         return (False, f"{e}")
 
@@ -34,7 +32,7 @@ def main() -> None:
     print()
 
     print("Using 'secure_archive' to read from an inaccessible file:")
-    print(secure_archive("/etc/passwd", "r"))
+    print(secure_archive("/etc/shadow", "r"))
     print()
 
     print("Using 'secure_archive' to read from a regular file:")
@@ -42,8 +40,23 @@ def main() -> None:
     print(memory)
     print()
 
-    print("Using 'secure_archive' to write previous content to a new file:")
-    print(secure_archive("memory.txt", "w", memory[1]))
+    if memory[0]:
+        print(
+            "Using 'secure_archive' to write previous content to a new file:")
+        print(secure_archive("test_memory.txt", "w", memory[1]))
+        print()
+    else:
+        print("ERROR: can't use 'secure_archive' "
+              "to write previous content to a new file.\n"
+              "Please create ../test.txt first")
+        print()
+
+    print("Using 'secure_archive' with unauthorized file mode for open():")
+    print(secure_archive("/etc/passwd", "rw"))
+    print()
+
+    print("Using 'secure_archive' to write with empty content:")
+    print(secure_archive("test_empty_write.txt", "w", ""))
     print()
 
 
