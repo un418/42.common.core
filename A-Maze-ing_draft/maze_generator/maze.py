a98fb18c86f84af018fd97f61596eff2604f5805
Adam Fernandes <adaferna@student.42lisboa.com>
2026-08-21 18:42:18 +0100

    A-Maze-ing draft -  wip

--- A-Maze-ing_draft/maze_generator/maze.py
+++ A-Maze-ing_draft/maze_generator/maze.py
@@ -3,22 +3,39 @@
 class Generator:
     ...
 
-class Grid:
-    def __init__(self, x_max:int, y_max: int) -> None:
-        self.x_max = x
-        self.y_max = y
-        self.matrix = [][]
-        self.border = False
+
+class Matrix:
+    def __init__(self, width: int, height: int) -> None:
+        self.width: int = width
+        self.height: int = height
+        self.matrix: list[list[Cell]] = [
+            [None for x in range(width)] for y in range(height)]
+
+    def gen_constraint(self) -> None:
+        """Add metadata to cell in the matrix before generating the wall"""
+        for y in range(self.height):
+            for x in range(self.width):
+                if (x == 0 or
+                        y == 0 or
+                        x == self.width - 1 or
+                        y == self.height - 1):
+                    self.matrix[y][x].border = True
+
+    def print_matrix(self) -> None:
+        ...
 
 
-class Case:
-    def __init__(self, x:int, y: int) -> None:
+class Cell:
+    def __init__(self, x: int, y: int, border: bool = False) -> None:
         self.x = x
         self.y = y
-        self.border = False
+        self.border = border
         self.logo = False
-        self.solution = False # part of 
+        self.wall: int | None = None
 
-        #self.visited = False #for maze solving
+        # self.solution = False # part of maze solution
+        # self.visited = False #for maze solving
 
 
+if __name__ == "__main__":
+    pass
